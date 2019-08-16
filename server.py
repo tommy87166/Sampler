6a6fe88c444697f6df3a3b2dfd23dce2c38873a0
tommy87166 <tommy87166@gmail.com>
2019-08-17 02:28:20 +0800

    run check in loop

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import asyncio
 import socketio
 import webbrowser
 import pandas
@@ -16,46 +17,11 @@ async def index(request):
         return web.Response(text=f.read().decode("utf8"), content_type='text/html')
 app.router.add_get('/', index)
 
-async def upload_handler(request):
-    """Handle Update File."""
-    reader = await request.multipart()
-
-    socket_field,file_field = False,False
-
-    async for field in reader:
-        if field.name == "socket":
-            socket_field = await field.text()
-        if field.name == "file":
-            file_field,size = BytesIO(),0
-            while True:
-                chunk = await field.read_chunk()
-                if not chunk:
-                    break
-                size += len(chunk)
-                file_field.write(chunk)
-            print("Received file in size of {}.".format(size))
-    
-    if socket_field and file_field:
-        await sio.emit('msg',"Received file in size of {}.".format(size), room=socket_field)
-        return web.Response(text="OK")
-    else:
-        return web.Response(text="Error")
-    
-app.router.add_post('/upload', upload_handler)
-
-@sio.event
-def connect(sid, environ):
-    print("connect ", sid)
-
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
-
-@sio.event
-async def openledger(sid, data):
+async def load_and_check(byte,sid):
     global ledger
-    df = pandas.read_excel(data)
-    await sio.emit('msg',"已開啟明細帳{}，共{}列{}欄".format(data,len(df),len(df.columns)), room=sid)
+    byte.seek(0)
+    df = pandas.read_excel(byte)
+    await sio.emit('msg',"已開啟明細帳，共{}列{}欄".format(len(df),len(df.columns)), room=sid)
     await sio.emit('msg',"進行檢查...", room=sid)
     try:
         assert "date"       in df,"未含有日期欄位:date"
@@ -76,18 +42,38 @@ async def openledger(sid, data):
         unique = ledger[["acc_no","acc_name"]].drop_duplicates().to_json(orient="records")
         await sio.emit('set_account',unique, room=sid)
 
+async def upload_handler(request):
+    """Handle Update File."""
+    reader = await request.multipart()
+    socket_field,file_field = False,False
+    async for field in reader:
+        if field.name == "socket":
+            socket_field = await field.text()
+        if field.name == "file":
+            file_field,size = BytesIO(),0
+            while True:
+                chunk = await field.read_chunk()
+                if not chunk:
+                    break
+                size += len(chunk)
+                file_field.write(chunk)
+    if socket_field and file_field:
+        await sio.emit('msg',"Received file in size of {}.".format(size), room=socket_field)
+        loop.create_task(load_and_check(file_field,socket_field))
+        return web.Response(text="OK")
+    else:
+        return web.Response(text="Error")
+app.router.add_post('/upload', upload_handler)
 
-#async def output(sid):
-#    await sio.emit('msg',df.to_json(),room=sid)
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
 
-class sampler(object):
-    def __init__(self,df,column,method,criteria,exclude=[]):
-        self.df       =  df
-        self.column   =  column
-        self.method   =  method
-        self.criteria =  criteria
-        self.exclude  =  exclude
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
     
 if __name__ == '__main__':
     host,port="127.0.0.1",8888
+    loop = asyncio.get_event_loop()
     web.run_app(app,host=host,port=port)
