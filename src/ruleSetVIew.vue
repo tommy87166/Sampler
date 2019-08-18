09c69ad3721149c0aff59eb459fe3fc4f84ae43b
tommy87166 <tommy87166@gmail.com>
2019-08-18 19:05:02 +0800

    finish rule set

--- src/ruleSetVIew.vue
+++ src/ruleSetVIew.vue
@@ -3,14 +3,40 @@
     <table class="table">
         <thead>
             <tr>
-            <th scope="col">#</th>
-            <th scope="col">名稱</th>
-            <th scope="col">方法</th>
-            <th scope="col">方向</th>
-            <th scope="col">參數</th>
+                <th scope="col">#</th>
+                <th scope="col">名稱</th>
+                <th scope="col">方法</th>
+                <th scope="col">方向</th>
+                <th scope="col">參數</th>
+                <th scope="col"></th>
             </tr>
         </thead>
         <tbody>
+            <tr>
+                <th scope="row"></th>
+                <td>
+                    <input v-model="name" class="form-control" required type="text" placeholder="名稱" />
+                </td>
+                <td>
+                    <select v-model="method" class="custom-select">
+                        <option value="order">個數</option>
+                        <option value="percent">比例</option>
+                    </select>
+                </td>
+                <td>
+                    <select v-model="direction" class="custom-select">
+                        <option value="debit">借方</option>
+                        <option value="credit">貸方</option>
+                    </select>
+                </td>
+                <td>
+                    <input v-model="criteria" class="form-control" min="0" required type="number" />
+                </td>
+                <td>
+                    <button v-on:click="addRule" type="button" class="btn btn-success">新增</button>
+                </td>
+            </tr>
+
             <tr v-for="(item, key, index) in rules">
                 <th scope="row">{{index + 1}}</th>
                 <td>{{item.name}}</td>
@@ -27,7 +53,10 @@
                     </select>
                 </td>
                 <td>
-                    <input class="form-control" min="0" required type="number" v-model.number="item.criteria" debounce="1000" />
+                    <input class="form-control" min="0" required type="number" v-model.number="item.criteria" />
+                </td>
+                <td>
+                    <button type="button" class="btn btn-danger" v-on:click="deleteRule" :data-name="item.name">刪除</button>
                 </td>
             </tr>
         </tbody>
@@ -38,7 +67,39 @@
 
 <script>
 module.exports = {
-  props: ['rules']
+  props: ['rules'],
+  data:function(){
+      return{
+        name:"",
+        method:"order",
+        direction:"debit",
+        criteria:3
+      }
+  },
+  methods:{
+      deleteRule:function(event){
+          var name = event.target.getAttribute("data-name")
+          console.log("Delete Rule",name)
+          this.$socket.emit("delete_rule",name)
+      },
+      addRule:function(){
+        console.log("Add Rule",this.name,this.method,this.direction,this.criteria)
+        console.log(this.rules)
+        if (this.name != "" && this.name.length > 0 && Object.keys(this.rules).indexOf(this.name) == -1 ){
+            var rule = {
+                "name"     : this.name, 
+                "method"   : this.method,
+                "direction": this.direction,
+                "criteria" : this.criteria,
+                "exclude"  : []
+            }
+            this.$socket.emit("set_rule",rule)
+        } else {
+            alert("名稱不得為空或已經存在")
+        }
+
+      }
+  }
 }
 </script>
 
