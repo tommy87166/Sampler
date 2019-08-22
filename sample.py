9a04763de078fd0fd8d62be2dacdcdfe0c833b61
tommy87166 <tommy87166@gmail.com>
2019-08-23 01:52:15 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,24 +6,54 @@ class AdvancedJSONEncoder(json.JSONEncoder):
         if hasattr(obj, '__jsonencode__'):
             return obj.__jsonencode__()
 
+class AdvancedJSONEncoderResult(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, '__jsonresult__'):
+            return obj.__jsonresult__()
+
 class sampler(object):
     def __init__(self,name,method,direction,criteria,exclude=[]):
         #Parameter
-        self.name      = name
-        self.method    = method
-        self.direction = direction
-        self.criteria  = criteria
-        self.exclude   = exclude
+        self.name       = name
+        self.method     = method
+        self.direction  = direction
+        self.criteria   = criteria
+        self.exclude    = exclude
+        self.multiplier = 12,12
         #Input
         self.df        = pandas.DataFrame()
         #Result
+        self.result    = pandas.DataFrame()
+    
+    #Properties
+    @property
+    def stat(self):
+        try:
+            population        =  self.df["debit"].sum() - self.df["credit"].sum()
+            population_expand =  population / self.multiplier[0] * self.multiplier[1]
+            size              =  self.result["debit"].sum() - self.result["credit"].sum()
+            ratio             =  size / population_expand
+            return population,population_expand,size,ratio
+        except KeyError:
+            return 0,0,0,0
+        
+    #Json Encorder
     def __jsonencode__(self):
         return {
-            "name"      : self.name, 
-            "method"    : self.method,
-            "direction" : self.direction,
-            "criteria"  : self.criteria,
-            "exclude"   : self.exclude
+            "name"       : self.name, 
+            "method"     : self.method,
+            "direction"  : self.direction,
+            "criteria"   : self.criteria,
+            "exclude"    : self.exclude,
+            "multiplier" : self.multiplier
         }
+    
+    def __jsonresult__(self):
+        return {
+            "df"         : self.df.to_dict(orient="records"),
+            "result"     : self.result.to_dict(orient="records"),
+            "parameter"  : self.__jsonencode__()
+        }
+    
     def sample(self,multiplier):
         pass
